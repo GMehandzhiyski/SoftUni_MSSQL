1f462cea32ace7ead6bef327ec7843b7ea4fd041
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-03 10:22:17 +0300

    07. Exercise - 100% DONE

--- 04 - Exercise -  Database Introduction/04.Exercise.sql
+++ 04 - Exercise -  Database Introduction/04.Exercise.sql
@@ -36,5 +36,34 @@
 			,(2, 'Bob', 15, 3)
 			,(3, 'Steward', NULL, 2)
  
+ --05. Truncate Table Minions
+  TRUNCATE TABLE [Minions]
+  
+  --06. Drop All Tables
+  DROP TABLE [Minions]
+  DROP TABLE [Towns]
+	DROP TABLE[People]
+  --07. Create Table People
+CREATE TABLE [People]
+  (
+	[Id]  INT PRIMARY KEY IDENTITY(1, 1)
+	,[Name] NVARCHAR(200) NOT NULL
+	,[Picture] VARBINARY(MAX)
+	,[Height] DECIMAL (3,2)
+	,[Weight] DECIMAL (5,2)
+	,[Gender] CHAR (1) NOT NULL
+				CHECK (Gender IN('m', 'f'))
+	,[Birthdate] DATETIME2 NOT NULL
+	,[Biography] VARCHAR(MAX)
+  )
 
-SELECT * FROM Minions
+INSERT INTO [People] ([Name], [Gender], [Birthdate])
+			VALUES ('Georgi', 'm', '1990-01-12')
+					,('Peter', 'm', '1991-01-12')
+					,('Ivana', 'f', '1991-12-12')
+					,('Penka', 'f', '1995-12-25')
+					,('Tosho', 'm', '1996-10-22')
+
+
+SELECT * FROM [People]
+DROP TABLE[People]
