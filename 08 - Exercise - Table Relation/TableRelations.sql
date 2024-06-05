d79edda0c1a82f4d32998f332ea87d1dfcf1c4af
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-05 10:22:34 +0300

    02.EX DONE

--- 08 - Exercise - Table Relation/TableRelations.sql
+++ 08 - Exercise - Table Relation/TableRelations.sql
@@ -24,3 +24,31 @@ INSERT INTO [Persons] ([FirstName], [Salary], [PassportID] )
 		VALUES('Roberto',43300,102),
 			 ('Tom',56100, 103),
 			 ('Yana', 60200,101)
+
+--02.
+CREATE TABLE [Manufacturers]
+(
+	[ManufacturerID] INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(20),
+	[EstablishedOn] DATETIME2
+)
+
+CREATE TABLE [Models]
+(
+	[ModelID] INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(30),
+	[ManufacturerID] INT FOREIGN KEY REFERENCES [Manufacturers]
+)
+
+INSERT INTO [Manufacturers]
+	VALUES ('BMW', '07/03/1916'),
+			('Tesla', '01/01/2003'),
+			('Lada', '01/05/1966')
+
+INSERT INTO[Models]
+	VALUES ('X1', 1),
+			('i6', 1),
+			('Model S', 2),
+			('Model X', 2),
+			('Model 3', 2),
+			('Nova', 3)
