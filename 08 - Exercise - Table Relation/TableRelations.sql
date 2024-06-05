f5032cf56b454d55e8b17cc42a9e229bac976555
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-05 10:36:37 +0300

    03.ex done

--- 08 - Exercise - Table Relation/TableRelations.sql
+++ 08 - Exercise - Table Relation/TableRelations.sql
@@ -52,3 +52,43 @@ INSERT INTO[Models]
 			('Model X', 2),
 			('Model 3', 2),
 			('Nova', 3)
+
+--03.
+CREATE TABLE [Students]
+(
+	[StudentID] INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(20) 
+)
+
+CREATE TABLE [Exams]
+(
+	[ExamID] INT PRIMARY KEY IDENTITY(101,1),
+	[Name] VARCHAR(20),
+)
+
+CREATE TABLE [StudentsExams]
+(
+	[StudentID] INT FOREIGN KEY REFERENCES [Students]([StudentID]),
+	[ExamID] INT FOREIGN KEY REFERENCES [Exams]([ExamID])
+	CONSTRAINT PK_StudentsExams PRIMARY KEY([StudentID],[ExamID]) -- COMPOSITE KEY
+)
+
+INSERT INTO [Students] 
+		VALUES('Mila'),
+			 ('Toni'),
+			 ('Ron')
+
+INSERT INTO [Exams] 
+		VALUES('SpringMVC'),
+			 ('Neo4j'),
+			 ('Oracle 11g')
+
+INSERT INTO [StudentsExams] 
+		VALUES(1,101),
+			 (1,102),
+			 (2,101),
+			 (3,103),
+			 (2,102),
+			 (2,103)
+	
+select * from StudentsExams
