d9b0ccad96a2bb6d6896a440a2b2bac93855398a
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-06 11:34:31 +0300

    17. ex done

--- 10 - Exercise - Built-in Functions/Built-in functions.sql
+++ 10 - Exercise - Built-in Functions/Built-in functions.sql
@@ -101,8 +101,27 @@ SELECT [UserName], SUBSTRING([Email], (CHARINDEX('@',[Email])+1), LEN([Email])) 
 FROM [Users]
 ORDER BY [EmailProveder], [UserName]
 
---15.
+--16.
 SELECT [UserName], [IpAddress]
 FROM [Users]
 WHERE [IpAddress] LIKE '___.1%.%.___'
 ORDER BY [UserName]
+
+--17.
+SELECT [Name] AS [Game], 
+		[Part of the Day] = 
+		CASE 
+			WHEN DATEPART(HOUR,[Start]) < 12 THEN 'Morning'
+			WHEN DATEPART(HOUR,[Start]) < 18 THEN 'Afternoon'
+			ELSE 'Evening'
+		END,
+		[Duration] = 
+		CASE
+			WHEN [Duration] <= 3 THEN 'Extra Short'
+			WHEN [Duration] <= 6 THEN 'Short'
+			WHEN [Duration] > 6 THEN 'Long'
+			ELSE 'Extra Long'
+		END
+FROM [Games]
+ORDER BY [Name], [Duration], [Part of the Day]
+
