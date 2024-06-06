054960ce31aba1107137007886d9632f7eeb89c4
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-06 10:49:50 +0300

    13. EX DONE

--- 10 - Exercise - Built-in Functions/Built-in functions.sql
+++ 10 - Exercise - Built-in Functions/Built-in functions.sql
@@ -81,3 +81,10 @@ SELECT [CountryName], [ISOCode]
 FROM [Countries]
 WHERE [CountryName] LIKE '%A%A%A%'
 ORDER BY [IsoCode]
+
+--13.
+SELECT  [PeakName], [RiverName],
+LOWER(CONCAT(SUBSTRING([PeakName], 1, LEN([PeakName])-1),[RiverName])) AS Mix
+FROM [Peaks], [Rivers]
+WHERE RIGHT([PeakName], 1) = LEFT([RiverName],1)
+ORDER BY Mix
