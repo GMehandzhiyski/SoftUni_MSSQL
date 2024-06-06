3c3ab48bf34b25dfcd9cd1ebbb3ff1a8f165d049
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-06 10:03:40 +0300

    08. EX DONE

--- 10 - Exercise - Built-in Functions/Built-in functions.sql
+++ 10 - Exercise - Built-in Functions/Built-in functions.sql
@@ -39,3 +39,10 @@ SELECT [TownID], [Name]
 FROM [Towns]
 WHERE [Name] NOT LIKE '[RBD]%'
 ORDER BY [Name]
+
+--08.
+CREATE VIEW V_EmployeesHiredAfter2000  AS
+
+	SELECT [FirstName], [LastName]
+	FROM [Employees]
+	WHERE DATEPART(YEAR, [HireDate]) > 2000
