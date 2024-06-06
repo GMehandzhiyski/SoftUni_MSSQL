4b8e5f9bcc93e5bf60aea20f75701160fa4506c1
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-06 10:34:53 +0300

    12 ex done

--- 10 - Exercise - Built-in Functions/Built-in functions.sql
+++ 10 - Exercise - Built-in Functions/Built-in functions.sql
@@ -72,3 +72,12 @@ SELECT * FROM
 ) AS [Result]
 WHERE [Result].[Rank] = 2
 ORDER BY [Salary] DESC
+
+
+
+USE [Geography]
+--12.
+SELECT [CountryName], [ISOCode]
+FROM [Countries]
+WHERE [CountryName] LIKE '%A%A%A%'
+ORDER BY [IsoCode]
