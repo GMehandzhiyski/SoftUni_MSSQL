15a7c21b84203cd639241f6967546b20dfab8af2
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-06 14:40:09 +0300

    02. lecure ex done

---
+++ 12 - Exercise Subqueries and Joins/Subqueries and Joins.sql
@@ -0,0 +1,18 @@
+--Lecture exercise
+--01.
+USE [SoftUni]
+
+SELECT TOP 50
+		e.[FirstName], e.[LastName], t.[Name] AS [Town], a.[AddressText]
+FROM [Employees] AS e
+JOIN [Addresses] AS a ON e.[AddressID] = a.[AddressID]
+JOIN [Towns] AS t ON a.[TownID] = t.[TownID]
+ORDER BY e.[FirstName], e.[LastName]
+
+
+--02.
+SELECT e.[FirstName], e.[LastName],e.[HireDate], d.[Name] AS [DeptName]
+FROM [Employees] AS e
+JOIN [Departments] AS d ON e.[DepartmentID] = d.[DepartmentID]
+WHERE d.[Name] IN('Sales', 'Finance')
+ORDER BY e.[HireDate]
