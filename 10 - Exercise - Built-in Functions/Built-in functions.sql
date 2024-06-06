ef9089750dce76346f4960c1917c541ec5196032
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-06 10:05:49 +0300

    09.ex done

--- 10 - Exercise - Built-in Functions/Built-in functions.sql
+++ 10 - Exercise - Built-in Functions/Built-in functions.sql
@@ -46,3 +46,8 @@ CREATE VIEW V_EmployeesHiredAfter2000  AS
 	SELECT [FirstName], [LastName]
 	FROM [Employees]
 	WHERE DATEPART(YEAR, [HireDate]) > 2000
+
+--09.
+SELECT [FirstName], [LastName]
+FROM [Employees]
+WHERE LEN([LastName]) = 5 
