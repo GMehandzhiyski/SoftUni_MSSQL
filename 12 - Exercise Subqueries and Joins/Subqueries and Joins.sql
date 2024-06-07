e84577b108bf6e6dc162d4cb2213ae17fdb04177
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-07 09:25:04 +0300

    04. ex done

--- 12 - Exercise Subqueries and Joins/Subqueries and Joins.sql
+++ 12 - Exercise Subqueries and Joins/Subqueries and Joins.sql
@@ -49,3 +49,12 @@ FROM
 	JOIN [Departments] AS d ON e.DepartmentID = d.DepartmentID
 WHERE d.[Name] = 'Sales'
 ORDER BY e.EmployeeID
+
+--04.
+SELECT TOP 5
+	e.[EmployeeID], e.[FirstName], e.[Salary], d.[Name] AS [DepartmentName]
+FROM
+	[Employees] AS e
+	JOIN [Departments] AS d ON e.[DepartmentID] = d.[DepartmentID]
+WHERE e.[Salary] > 15000
+ORDER BY d.[DepartmentID]
