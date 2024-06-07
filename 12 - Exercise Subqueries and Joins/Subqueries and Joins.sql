902f472eb28618f9e72a42ff6ed594890b936615
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-07 11:37:26 +0300

    13. ex done

--- 12 - Exercise Subqueries and Joins/Subqueries and Joins.sql
+++ 12 - Exercise Subqueries and Joins/Subqueries and Joins.sql
@@ -152,4 +152,17 @@ FROM
 	JOIN [Peaks] AS p ON m.Id = p.MountainId
 WHERE c.[CountryName] = 'Bulgaria'
 		AND p.[Elevation] > 2835
-ORDER BY p.[Elevation] DESC
+ORDER BY 
+	p.[Elevation] DESC
+
+--13.
+SELECT
+	c.[CountryCode],
+	COUNT(m.[MountainRange]) AS [MountainRanges]
+FROM
+	[Countries] AS c 
+	JOIN [MountainsCountries] AS mc ON c.[CountryCode] = mc.[CountryCode]
+	JOIN [Mountains] AS m ON mc.[MountainId] = m.Id 
+WHERE c.[CountryName] IN ('United States','Russia', 'Bulgaria')
+GROUP BY
+	c.[CountryCode]
