d3d44fea173fd69f5abb1dedba6176a13a892ff7
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-07 07:14:23 +0300

    02. EX DONE

--- 12 - Exercise Subqueries and Joins/Subqueries and Joins.sql
+++ 12 - Exercise Subqueries and Joins/Subqueries and Joins.sql
@@ -23,4 +23,21 @@ WHERE
 ORDER BY
 	e.[HireDate]
 
+--Exercise
+--01.
+SELECT TOP 5
+	e.[EmployeeID],e.[JobTitle], a.[AddressID], a.[AddressText] 
+FROM 
+	[Employees] AS e
+	JOIN [Addresses] AS a ON e.AddressID = a.[AddressID]
+ORDER BY a.[AddressID]
+
+--02. 
+SELECT TOP 50
+	e.[FirstName], e.[LastName], t.[Name] AS [Town], a.[AddressText]
+FROM
+	[Employees] AS e
+	JOIN [Addresses] AS a ON e.AddressID = a.AddressID
+	JOIN [Towns] AS t ON t.TownID = a.TownID
+ORDER BY [FirstName], [LastName]
 
