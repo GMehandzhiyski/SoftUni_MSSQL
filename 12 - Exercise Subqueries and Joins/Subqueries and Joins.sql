6c6bd858e2969a3c0b5a84b4e313f42e832ca165
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-07 11:23:49 +0300

    12. ex done

--- 12 - Exercise Subqueries and Joins/Subqueries and Joins.sql
+++ 12 - Exercise Subqueries and Joins/Subqueries and Joins.sql
@@ -139,3 +139,17 @@ FROM
 GROUP BY d.DepartmentID
 ORDER BY
 	[MinAverageSalary]
+
+USE [Geography]
+
+--12.
+SELECT
+	c.[CountryCode], m.[MountainRange], p.[PeakName], p.[Elevation]
+FROM
+	[Countries]	AS c 
+	JOIN  [MountainsCountries] AS mc ON c.[CountryCode] = mc.[CountryCode]
+	JOIN [Mountains] AS m ON mc.MountainId = m.Id
+	JOIN [Peaks] AS p ON m.Id = p.MountainId
+WHERE c.[CountryName] = 'Bulgaria'
+		AND p.[Elevation] > 2835
+ORDER BY p.[Elevation] DESC
