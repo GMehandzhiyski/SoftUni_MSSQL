bff3b43f093f4dcfa67468ac2f6480dda27191e8
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-08 08:05:32 +0300

    14.ex done

--- 14 - Exercise Indices and Data Aggregation/IndecisAndDataAggregation.sql
+++ 14 - Exercise Indices and Data Aggregation/IndecisAndDataAggregation.sql
@@ -140,7 +140,17 @@ GROUP BY
 		[DepartmentID]
 ORDER BY 
 	[DepartmentID]
-	
+
+--14.
+SELECT
+	[DepartmentID],
+	MIN(Salary) AS [MinimumSalary]
+FROM
+	[Employees]
+WHERE [DepartmentID] IN (2, 5, 7)
+		AND [HireDate] > '2000-01-01'
+GROUP BY
+	[DepartmentID]
 
 	
 
