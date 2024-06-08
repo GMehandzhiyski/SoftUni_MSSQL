861ff969030b2d6d969986f91a8c9badf5708a46
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-08 06:32:14 +0300

    03. ex done

--- 14 - Exercise Indices and Data Aggregation/IndecisAndDataAggregation.sql
+++ 14 - Exercise Indices and Data Aggregation/IndecisAndDataAggregation.sql
@@ -3,3 +3,18 @@ USE [Gringotts]
 SELECT COUNT(*) AS [Count]
 FROM
 	[WizzardDeposits]
+
+--02.
+SELECT 
+	MAX([MagicWandSize]) AS [LongestMagicWand]
+FROM 
+	[WizzardDeposits]
+
+
+--03.
+SELECT 
+	[DepositGroup],
+	MAX([MagicWandSize]) AS [LongestMagicWand]
+FROM 
+	[WizzardDeposits]
+GROUP BY [DepositGroup]
