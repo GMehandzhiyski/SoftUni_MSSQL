058400c5ab07fab8d65cc8f7584c54dc98dd0baa
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-09 08:21:35 +0300

    06. ex done

--- 16 - Exercise Functions and Procedures/TrigerAndTransactions.sql
+++ 16 - Exercise Functions and Procedures/TrigerAndTransactions.sql
@@ -83,3 +83,19 @@ SELECT
 	dbo.ufn_GetSalaryLevel(Salary)
 FROM
 	[Employees]
+
+
+--06.
+CREATE PROCEDURE usp_EmployeesBySalaryLevel @salaryLevel VARCHAR(10)
+AS
+BEGIN
+	SELECT 
+		[FirstName],
+		[LastName]
+	FROM 
+		[Employees]
+	WHERE dbo.ufn_GetSalaryLevel([Salary]) = @salaryLevel
+	
+END
+
+EXEC usp_EmployeesBySalaryLevel high
