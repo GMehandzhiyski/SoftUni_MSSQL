a1701ebf4c492611211cf1c2ce880dac5c1e9c2b
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-10 15:06:30 +0300

    09. ex done

--- 20 -ExamPrapar/01.Regular Exam - 15 Oct 2023/RegularExam.sql
+++ 20 -ExamPrapar/01.Regular Exam - 15 Oct 2023/RegularExam.sql
@@ -151,3 +151,34 @@ FROM
 WHERE b.[TouristId] IS NULL
 ORDER BY 
 	t.[Name]
+
+---------------------------------------------------------------------
+SELECT 
+	[Id],
+	[Name],
+	[PhoneNumber]
+FROM
+	[Tourists]
+WHERE [Id] NOT IN (SELECT [TouristId] FROM [Bookings] )
+ORDER BY 
+	[Name]
+
+SELECT * FROM Bookings
+
+--08.
+SELECT TOP 10
+	h.[Name] AS [HotelName],
+	d.[Name] AS [DestinationName],
+	c.[Name] AS [CountryName]
+FROM
+	[Bookings] AS b
+	JOIN [Hotels] AS h ON b.[HotelId] = h.[Id]
+	JOIN [Destinations] AS d ON h.DestinationId = d.Id
+	JOIN [Countries] AS c ON d.CountryId = c.Id
+WHERE [ArrivalDate] < '2023-12-31'
+		AND h.[Id] % 2 != 0
+ORDER BY 
+	c.[Name],
+	b.[ArrivalDate]
+
+select * from Countries
