4ac3278a1c5fc30ec5ed52cf7432f12f5f03560c
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-10 15:30:13 +0300

    11. EX DONE

--- 20 -ExamPrapar/01.Regular Exam - 15 Oct 2023/RegularExam.sql
+++ 20 -ExamPrapar/01.Regular Exam - 15 Oct 2023/RegularExam.sql
@@ -195,3 +195,16 @@ FROM
 WHERE t.[Name] NOT LIKE '%EZ' 
 ORDER BY
 	r.[Price] DESC
+
+--10.
+SELECT 
+	h.[Name],
+	SUM(r.[Price]*(DATEDIFF(DAY, [ArrivalDate], [DepartureDate]))) AS [HotelRevenue]
+FROM
+	[Bookings] AS b
+	JOIN [Hotels] AS h ON b.HotelId = h.Id
+	JOIN [Rooms] AS r ON b.RoomId = r.Id
+GROUP BY
+	h.[Name]
+ORDER BY 
+	[HotelRevenue] DESC
