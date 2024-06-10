1c4ea6439bfe465f90965a9dd82856decd69cafb
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-10 14:50:05 +0300

    07. ex done

--- 20 -ExamPrapar/01.Regular Exam - 15 Oct 2023/RegularExam.sql
+++ 20 -ExamPrapar/01.Regular Exam - 15 Oct 2023/RegularExam.sql
@@ -140,3 +140,14 @@ GROUP BY
 ORDER BY
 	COUNT(*) DESC;
 	
+--07.
+SELECT 
+	t.[Id],
+	t.[Name],
+	t.[PhoneNumber]
+FROM
+	[Tourists] AS t
+	LEFT JOIN [Bookings] AS b ON t.Id = b.TouristId
+WHERE b.[TouristId] IS NULL
+ORDER BY 
+	t.[Name]
