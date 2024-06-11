c91d579e5507a8d89b6dc5575ff07bd429b650f6
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-11 13:49:19 +0300

    03. EX DONE

--- 20 -ExamPrapar/02.Regular Exam - 19 June 2022/SQLQuery3.sql
+++ 20 -ExamPrapar/02.Regular Exam - 19 June 2022/SQLQuery3.sql
@@ -2,64 +2,63 @@ CREATE DATABASE Zoo
 USE Zoo
 GO
 
-
-CREATE TABLE Owners
-(
-	[Id] INT PRIMARY KEY IDENTITY,
-	[Name] VARCHAR(50) NOT NULL,
-	[PhoneNumber] VARCHAR(15) NOT NULL,
-	[Address] VARCHAR(50)
+--01.
+CREATE TABLE Owners(
+Id INT IDENTITY PRIMARY KEY,
+Name VARCHAR(50) NOT NULL,
+PhoneNumber VARCHAR(15) NOT NULL,
+Address VARCHAR(50)
 )
 
-CREATE TABLE AnimalTypes
-(
-	[Id] INT PRIMARY KEY IDENTITY,
-	[AnimalType] VARCHAR(30) NOT NULL
+CREATE TABLE AnimalTypes(
+Id INT IDENTITY PRIMARY KEY,
+AnimalType VARCHAR(30) NOT NULL
 )
 
-CREATE TABLE Cages
-(
-	[Id] INT PRIMARY KEY IDENTITY,
-	[AnimalTypeId] INT NOT NULL FOREIGN KEY REFERENCES [AnimalTypes](Id)
+CREATE TABLE Cages(
+Id INT IDENTITY PRIMARY KEY,
+AnimalTypeId INT NOT NULL FOREIGN KEY REFERENCES AnimalTypes(Id),
 )
 
-CREATE TABLE Animals
-(
-	[Id] INT PRIMARY KEY IDENTITY,
-	[Name] VARCHAR(30) NOT NULL,
-	[BirthDate] DATE NOT NULL,
-	[OwnerId] INT FOREIGN KEY REFERENCES [Owners](Id),
-	[AnimalTypeId] INT NOT NULL FOREIGN KEY REFERENCES [AnimalTypes](Id)
+CREATE TABLE Animals(
+Id INT IDENTITY PRIMARY KEY,
+Name VARCHAR(30) NOT NULL,
+BirthDate DATE NOT NULL,
+OwnerId INT FOREIGN KEY REFERENCES Owners(Id),
+AnimalTypeId INT NOT NULL FOREIGN KEY REFERENCES AnimalTypes(Id)
 )
 
-CREATE TABLE AnimalsCages
-(
-	[CageId] INT NOT NULL FOREIGN KEY REFERENCES [Cages](Id),
-	[AnimalId] INT NOT NULL FOREIGN KEY REFERENCES [Animals](Id),
-	PRIMARY KEY([CageId],[AnimalId] )
+CREATE TABLE AnimalsCages(
+CageId INT NOT NULL FOREIGN KEY REFERENCES Cages(Id),
+AnimalId INT NOT NULL FOREIGN KEY REFERENCES Animals(Id),
+PRIMARY KEY (CageId, AnimalId)
 )
 
-
-CREATE TABLE VolunteersDepartments
-(
-	[Id] INT PRIMARY KEY IDENTITY,
-	[DepartmentName] VARCHAR(30) NOT NULL
+CREATE TABLE VolunteersDepartments(
+Id INT IDENTITY PRIMARY KEY,
+DepartmentName VARCHAR(30) NOT NULL
 )
 
-CREATE TABLE Volunteers
-(
-	[Id] INT PRIMARY KEY IDENTITY,
-	[Name] VARCHAR(50) NOT NULL,
-	[PhoneNumber] VARCHAR(15) NOT NULL,
-	[Address] VARCHAR(50),
-	[AnimalId]	INT FOREIGN KEY REFERENCES [Animals](Id),
-	[DepartmentId] INT NOT NULL FOREIGN KEY REFERENCES [VolunteersDepartments](Id)
+CREATE TABLE Volunteers(
+Id INT IDENTITY PRIMARY KEY,
+Name VARCHAR(50) NOT NULL,
+PhoneNumber VARCHAR(15) NOT NULL,
+Address VARCHAR(50),
+AnimalId INT FOREIGN KEY REFERENCES Animals(Id),
+DepartmentId INT NOT NULL FOREIGN KEY REFERENCES VolunteersDepartments(Id)
 )
 
-DROP TABLE Volunteers
-DROP TABLE VolunteersDepartments
-DROP TABLE AnimalsCages
-DROP TABLE Animals
-DROP TABLE Cages
-DROP TABLE AnimalTypes
-DROP TABLE Owners
+--02. 
+INSERT INTO Volunteers(Name, PhoneNumber, Address, AnimalId, DepartmentId) VALUES
+('Anita Kostova', '0896365412', 'Sofia, 5 Rosa str.', 15, 1),
+('Dimitur Stoev', '0877564223', NULL, 42, 4),
+('Kalina Evtimova', '0896321112', 'Silistra, 21 Breza str.', 9, 7),
+('Stoyan Tomov', '0898564100', 'Montana, 1 Bor str.', 18, 8),
+('Boryana Mileva', '0888112233', NULL, 31, 5)
+
+INSERT INTO Animals(Name, BirthDate, OwnerId, AnimalTypeId) VALUES
+('Giraffe', '2018-09-21', 21, 1),
+('Harpy Eagle', '2015-04-17', 15, 3),
+('Hamadryas Baboon', '2017-11-02', NULL, 1),
+('Tuatara', '2021-06-30', 2, 4)
+
