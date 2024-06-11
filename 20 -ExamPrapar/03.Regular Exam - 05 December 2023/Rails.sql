bb8da9c2eeb76f1d71a3fdcb98d8e305f840637c
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-11 10:54:16 +0300

    01 EX DONE

---
+++ 20 -ExamPrapar/03.Regular Exam - 05 December 2023/Rails.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE RailwaysDb
+USE  RailwaysDb
+GO
+
+
+CREATE TABLE Passengers
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(80) NOT NULL
+);
+
+CREATE TABLE Towns
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE RailwayStations
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL,
+	TownId INT  NOT NULL,
+	FOREIGN KEY (TownId) REFERENCES Towns(Id)
+);
+
+CREATE TABLE Trains
+(
+	Id INT PRIMARY KEY IDENTITY,
+	HourOfDeparture VARCHAR(5) NOT NULL,
+	HourOfArrival VARCHAR(5) NOT NULL,
+	DepartureTownId INT NOT NULL,
+	FOREIGN KEY (DepartureTownId) REFERENCES Towns(Id),
+	ArrivalTownId INT NOT NULL, 
+	FOREIGN KEY (ArrivalTownId) REFERENCES Towns(Id)
+);
+
+CREATE TABLE TrainsRailwayStations
+(
+	TrainId INT NOT NULL,
+	RailwayStationId INT NOT NULL,
+	PRIMARY KEY(TrainId,RailwayStationId),
+	FOREIGN KEY (TrainId) REFERENCES Trains(Id),
+	FOREIGN KEY (RailwayStationId) REFERENCES RailwayStations(Id)
+
+);
+
+CREATE TABLE MaintenanceRecords
+(
+	Id INT PRIMARY KEY IDENTITY,
+	DateOfMaintenance DATE NOT NULL,
+	Details VARCHAR(2000) NOT NULL,
+	TrainId INT NOT NULL ,
+	FOREIGN KEY (TrainId) REFERENCES Trains(Id)
+);
+
+CREATE TABLE Tickets
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Price DECIMAL(18,2) NOT NULL,
+	DateOfDeparture DATE NOT NULL,
+	DateOfArrival DATE NOT NULL,
+	TrainId INT NOT NULL,
+	FOREIGN KEY (TrainId) REFERENCES Trains(Id),
+	PassengerId INT NOT NULL,
+	FOREIGN KEY (PassengerId) REFERENCES Passengers(Id)
+);
