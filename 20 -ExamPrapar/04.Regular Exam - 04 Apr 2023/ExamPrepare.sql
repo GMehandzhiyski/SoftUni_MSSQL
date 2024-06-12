6134fe3f9c0cdabd6c0dd088d608e6a8904ba32f
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-12 11:20:52 +0300

    10. EX DONE

--- 20 -ExamPrapar/04.Regular Exam - 04 Apr 2023/ExamPrepare.sql
+++ 20 -ExamPrapar/04.Regular Exam - 04 Apr 2023/ExamPrepare.sql
@@ -218,3 +218,22 @@ GROUP BY
 	c.NumberVAT
 ORDER BY
 	MAX(p.Price) DESC
+
+--10.
+SELECT
+	c.[Name] AS Client,
+	FLOOR(AVG(p.Price)) AS [Average Price]
+FROM
+	Clients AS c
+	JOIN ProductsClients AS pc ON c.Id = pc.ClientId
+	JOIN Products AS p ON pc.ProductId = p.Id
+	JOIN Vendors AS v ON p.VendorId = v.Id
+WHERE pc.ClientId IS NOT NULL
+	AND v.NumberVAT LIKE '%FR%'
+GROUP BY 
+	c.[Name]
+ORDER BY
+	AVG(p.Price),
+	c.[Name] DESC
+
+
