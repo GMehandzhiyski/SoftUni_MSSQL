155886f94961db0e545d9f7e2b57786e2e47caeb
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-12 13:22:20 +0300

    08. EX DONE

--- 20 -ExamPrapar/05.Regular Exam -  19 Feb 2023/FEB2023.sql
+++ 20 -ExamPrapar/05.Regular Exam -  19 Feb 2023/FEB2023.sql
@@ -153,4 +153,18 @@ FROM
 WHERE cb.BoardgameId IS NULL
 ORDER BY
 	CONCAT_WS(' ',c.FirstName, c.LastName)
-	
+
+--08.
+SELECT TOP 5
+	b.[Name],
+	b.Rating,
+	C.[Name] AS CategoryName
+FROM 
+	Boardgames AS b
+	JOIN PlayersRanges AS pr ON b.PlayersRangeId = pr.Id
+	JOIN Categories AS c ON b.CategoryId = c.Id
+WHERE (b.Rating > 7.00 AND b.[Name] LIKE '%a%')
+		OR (b.Rating > 7.50 AND (pr.PlayersMin = 2 AND pr.PlayersMax = 5))
+ORDER BY
+	b.[Name],
+	b.Rating DESC
