cbc3f856be8077265e816ea8b5fc80824a9f6980
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-12 15:59:54 +0300

    10. EX DONE

--- 20 -ExamPrapar/06.Regular Exam  - 10 Aug 2022/Aug2022.sql
+++ 20 -ExamPrapar/06.Regular Exam  - 10 Aug 2022/Aug2022.sql
@@ -180,3 +180,20 @@ FROM
 	LEFT JOIN BonusPrizes AS bp ON tb.BonusPrizeId = bp.Id
 ORDER BY
 	t.Name
+
+--10.
+SELECT 
+	DISTINCT SUBSTRING(t.Name, CHARINDEX(' ',t.Name)+1, LEN(t.Name)),
+	t.Nationality,
+	t.Age,
+	t.PhoneNumber
+FROM
+	Tourists AS t
+	JOIN SitesTourists AS st ON t.Id = st.TouristId
+	JOIN Sites AS s ON st.SiteId = s.Id
+	JOIN Categories AS c ON s.CategoryId = c.Id
+WHERE c.Name = 'History and archaeology'
+ORDER BY
+	SUBSTRING(t.Name, CHARINDEX(' ',t.Name)+1, LEN(t.Name))
+
+SELECT * FROM Categories
