30eb6dc7a227528ff0564e9a49575b34098724a9
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-13 14:47:28 +0300

    06. EX DONE

--- 20 -ExamPrapar/08.Regular Exam - 16 Oct 2021/Oct2021.sql
+++ 20 -ExamPrapar/08.Regular Exam - 16 Oct 2021/Oct2021.sql
@@ -126,3 +126,19 @@ ORDER BY
 	PriceForSingleCigar,
 	CigarName DESC
 
+--06.
+SELECT
+	c.Id,
+	c.CigarName,
+	c.PriceForSingleCigar,
+	t.TasteType,
+	t.TasteStrength
+
+FROM
+	Cigars AS c
+	JOIN Tastes AS t ON c.TastId = t.Id
+WHERE t.TasteType = 'Earthy'
+		OR t.TasteType = 'Woody'
+ORDER BY
+	PriceForSingleCigar DESC
+
