3e311bdf1ffb9c6a29e9f1b7ff624a46e42c3314
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-13 13:30:59 +0300

    12, EX DONE

--- 20 -ExamPrapar/07.Regular Exam - 10 Dec 2021/Dec2021.sql
+++ 20 -ExamPrapar/07.Regular Exam - 10 Dec 2021/Dec2021.sql
@@ -220,3 +220,69 @@ END
 SELECT dbo.udf_FlightDestinationsByEmail ('PierretteDunmuir@gmail.com')
 SELECT dbo.udf_FlightDestinationsByEmail('Montacute@gmail.com')
 SELECT dbo.udf_FlightDestinationsByEmail('MerisShale@gmail.com')
+
+
+
+--12.
+CREATE OR ALTER PROCEDURE usp_SearchByAirportName(@airportName VARCHAR(70))
+AS
+BEGIN
+    SELECT
+        ap.AirportName,
+        p.FullName,
+        CASE 
+            WHEN fd.TicketPrice <= 400 THEN 'Low'
+            WHEN fd.TicketPrice BETWEEN 401 AND 1500 THEN 'Medium'
+            ELSE 'High'
+        END AS LevelOfTicketPrice,
+        ac.Manufacturer,
+        ac.Condition,
+        aty.TypeName
+    FROM
+        Airports AS ap
+    JOIN FlightDestinations AS fd ON ap.Id = fd.AirportId
+    JOIN Passengers AS p ON fd.PassengerId = p.Id
+    JOIN Aircraft AS ac ON fd.AircraftId = ac.Id
+    JOIN AircraftTypes AS aty ON ac.TypeId = aty.Id
+    WHERE ap.AirportName = @airportName
+    ORDER BY
+        ac.Manufacturer,
+        p.FullName
+
+END
+
+EXEC usp_SearchByAirportName 'Sir Seretse Khama International Airport'	
+-------------------------------------------------------------------------------
+
+SELECT
+	fd.TicketPrice
+FROM
+	Airports AS ap
+	JOIN FlightDestinations AS fd  ON ap.Id = fd.AirportId
+WHERE AirportName = 'Sir Seretse Khama International Airport'
+
+
+
+
+
+-----------------------------------------------------------------------------------
+
+SELECT
+	ap.AirportName,
+	p.FullName,
+	----LevelOfTickerPrice 
+	ac.Manufacturer,
+	ac.Condition,
+	aty.TypeName
+
+FROM
+	Airports AS ap
+	JOIN FlightDestinations AS fd  ON ap.Id = fd.AirportId
+	JOIN Passengers AS p ON fd.PassengerId = p.Id
+	JOIN Aircraft AS ac ON fd.AircraftId = ac.Id
+	JOIN AircraftTypes AS aty ON ac.TypeId = aty.Id
+WHERE AirportName = 'Sir Seretse Khama International Airport'
+		AND fd.TicketPrice <= 400
+ORDER BY
+	ac.Manufacturer,
+	p.FullName
