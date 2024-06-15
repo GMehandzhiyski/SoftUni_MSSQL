1e954d09f3c5aa862d77467e0e6301db2b0b11c3
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-06-15 09:44:47 +0300

    12. EX DONE

--- 20 -ExamPrapar/12.Regular Exam - 15 Oct 2023 - Copy/Oct2023.sql
+++ 20 -ExamPrapar/12.Regular Exam - 15 Oct 2023 - Copy/Oct2023.sql
@@ -187,5 +187,45 @@ GROUP BY
 ORDER BY
 	SUM(r.Price * DATEDIFF(DAY, ArrivalDate, DepartureDate)) DESC
 
-	
+--11.
+CREATE FUNCTION udf_RoomsWithTourists(@name VARCHAR(30))
+RETURNS INT
+AS
+BEGIN
+	DECLARE @result INT
+	SELECT
+		@result = SUM(b.AdultsCount + b.ChildrenCount)
+	FROM
+		Tourists AS t
+		JOIN Bookings AS b ON t.Id = b.TouristId
+		JOIN Hotels AS h ON b.HotelId = h.Id
+		JOIN Rooms AS r ON b.RoomId = r.Id
+	WHERE r.Type = @name
+	RETURN @result
+END
+SELECT dbo.udf_RoomsWithTourists('Double Room')
 
+--12.
+CREATE PROCEDURE usp_SearchByCountry(@country VARCHAR(40)) 
+AS
+BEGIN
+	SELECT 
+		t.Name,
+		t.PhoneNumber,
+		t.Email,
+		COUNT(B.RoomId)
+	FROM
+		Tourists AS t
+		JOIN Bookings AS b ON t.Id = b.TouristId
+		JOIN Countries AS c ON t.CountryId = c.Id
+	WHERE c.Name = @country
+	GROUP BY
+		t.Name,
+		t.PhoneNumber,
+		t.Email
+	ORDER BY
+		t.Name,
+		COUNT(B.RoomId) DESC
+END
+
+EXEC usp_SearchByCountry 'Greece'
